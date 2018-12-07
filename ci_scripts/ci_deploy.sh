82f836de97a3deccd5438b5bf266a96e892d0f8d
David Wybourn <dwybourn@equalexperts.com>
2018-12-07 16:28:17 +0000

    HTBHF-5 Added ci integration scripts with accompanying directories and files. Service is now bound to a postgres service. Updated codebase to use java 11

---
+++ ci_scripts/ci_deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# if this is a pull request or branch (non-master) build, then just exit
+echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST, TRAVIS_BRANCH=$TRAVIS_BRANCH"
+if [[ "$TRAVIS_PULL_REQUEST" != "false"  || "$TRAVIS_BRANCH" != "master" ]]; then
+   echo "Not deploying pull request or branch build"
+   exit
+fi
+
+check_variable_is_set(){
+    if [[ -z ${!1} ]]; then
+        echo "$1 must be set and non empty"
+        exit 1
+    fi
+}
+
+check_variable_is_set BIN_DIR
+check_variable_is_set DEPLOY_SCRIPTS_URL
+check_variable_is_set DEPLOY_SCRIPT_VERSION
+
+echo "Installing deploy scripts"
+if [[ ! -e ${BIN_DIR}/deploy_scripts_${DEPLOY_SCRIPT_VERSION} ]]; then
+    mkdir -p ${BIN_DIR}
+    cd ${BIN_DIR}
+    wget "${DEPLOY_SCRIPTS_URL}/${DEPLOY_SCRIPT_VERSION}.zip" -q -O deploy_scripts.zip && unzip -j -o deploy_scripts.zip && rm deploy_scripts.zip
+    touch deploy_scripts_${DEPLOY_SCRIPT_VERSION}
+    cd ..
+fi
+
+/bin/bash ${BIN_DIR}/deploy.sh
---
+++ ci_scripts/smoke_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Running smoke tests against $1"
+cd smoke_tests
+./gradlew clean test -Dbase_url=$1
