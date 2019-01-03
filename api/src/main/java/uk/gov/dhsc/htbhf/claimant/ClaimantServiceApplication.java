c137a8afdad507fed91aa33894391428e2f20303
Matthew Smith <msmith@equalexperts.com>
2019-01-03 17:16:19 +0000

    HTBHF-114 Removed Databasemigrator code and relying upon spring boot to invoke flyway for us

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/ClaimantServiceApplication.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/ClaimantServiceApplication.java
@@ -1,6 +1,5 @@
 package uk.gov.dhsc.htbhf.claimant;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,18 +8,11 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.dhsc.htbhf.claimant.requestcontext.RequestContext;
 
-import javax.sql.DataSource;
-
 @SpringBootApplication
 public class ClaimantServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClaimantServiceApplication.class, args);
-    }
-
-    @Bean
-    public Flyway flyway(DataSource dataSource) {
-        return Flyway.configure().dataSource(dataSource).load();
     }
 
     @Bean
--- ci_scripts/smoke_tests.sh
+++ ci_scripts/smoke_tests.sh
@@ -2,4 +2,4 @@
 
 echo "Running smoke tests against $1"
 cd smoke_tests
-./gradlew clean test -Dbase_url=$1
+./gradlew clean test -Dbase_url=$1 --info
