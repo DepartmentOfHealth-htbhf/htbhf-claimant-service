16ea3f2fec64b046da91a6017f25ba7a2399876f
Matthew Smith <msmith@equalexperts.com>
2019-01-03 14:59:54 +0000

    HTBHF-115 Fixed smoke tests to include protocol if not provided as part of base_url

--- smoke_tests/src/test/groovy/uk/gov/dhsc/htbhf/smoke/ServiceIsRunningSpec.groovy
+++ smoke_tests/src/test/groovy/uk/gov/dhsc/htbhf/smoke/ServiceIsRunningSpec.groovy
@@ -9,11 +9,13 @@ class ServiceIsRunningSpec extends Specification {
     @Shared
     String baseUrl = System.getProperty("base_url") == null ? "http://localhost:8080" : System.getProperty("base_url")
     @Shared
-    def client = new RESTClient(baseUrl)
+    String protocol = baseUrl.startsWith("http") ? "" : "https://"
+    @Shared
+    def client = new RESTClient(protocol + baseUrl)
 
     def "Health check status is UP"() {
         given: "The base url of the api"
-        println("Running health check against base_url=$baseUrl")
+        println("Running health check against $protocol$baseUrl")
 
         when: "a rest call is performed to the health check"
         def response = client.get(path : "/actuator/health")
