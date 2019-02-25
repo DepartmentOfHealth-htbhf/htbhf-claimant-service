995425bae2740b4d0cd6b7d033ae66aeb49f7a27
Matt Smith <msmith@equalexperts.com>
2019-02-25 11:03:15 +0000

    HTBHF-541 added variable-service as a service in manifest.yml, added logging of application info at startup

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/ApiDocumentation.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/ApiDocumentation.java
@@ -1,9 +1,12 @@
 package uk.gov.dhsc.htbhf.claimant;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,13 +19,21 @@ import static java.util.Collections.emptyList;
 
 /**
  * Definitions of SpringFox beans to generate Swagger documentation.
+ * Also logs application info at startup.
  */
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class) // enable documentation of JSR-305 constraints
+@Slf4j
 public class ApiDocumentation {
 
     @Value("${app.version:}") // use APP_VERSION env variable if available, otherwise give no version info
     private String appVersion;
+
+    @Value("${instance.index:}") // use INSTANCE_INDEX env variable if available, otherwise give no index info
+    private String instanceIndex;
+
+    @Value("${vcap.application.application_id:}") // the id of the application as provided by cf
+    private String applicationId;
 
     @Bean
     public Docket apiDocket() {
@@ -49,4 +60,8 @@ public class ApiDocumentation {
 
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void logAfterStartup() {
+        log.info("Application started. App version={}, app id={}, instance index={}", appVersion, applicationId, instanceIndex);
+    }
 }
