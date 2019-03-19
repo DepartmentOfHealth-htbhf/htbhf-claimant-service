35c9e24b06aa5fb8422fc2623b2b4ad023adf535
Gethyn Jones <gethyn1@gmail.com>
2019-03-19 16:16:33 +0000

    HTBHF-641 remove unnecessary annotation from eligibility status

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/model/eligibility/EligibilityStatus.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/model/eligibility/EligibilityStatus.java
@@ -1,13 +1,10 @@
 package uk.gov.dhsc.htbhf.claimant.model.eligibility;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 
 /**
- * The possible states that a claim can be in according to the DWP.
- * The value of ERROR has been added for when there are problems connecting to DWP.
+ * The possible states that a claim can be in.
  */
-@AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum EligibilityStatus {
 
