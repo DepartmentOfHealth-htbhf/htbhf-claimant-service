29c662afce43c48145e027595f8bbd70223bda6e
Gethyn Jones <gethyn1@gmail.com>
2019-03-19 16:32:53 +0000

    HTBHF-641 update flyway script to set default eligibility status to INELIGIBLE

--- db/src/main/resources/db/migration/V1_002__add_status_column.sql
+++ db/src/main/resources/db/migration/V1_002__add_status_column.sql
@@ -1,5 +1,5 @@
 alter table claimant ADD COLUMN eligibility_status varchar(20);
 
-update claimant set eligibility_status = 'ELIGIBLE' where eligibility_status is null;
+update claimant set eligibility_status = 'INELIGIBLE' where eligibility_status is null;
 
 alter table claimant ALTER COLUMN eligibility_status SET not null;
