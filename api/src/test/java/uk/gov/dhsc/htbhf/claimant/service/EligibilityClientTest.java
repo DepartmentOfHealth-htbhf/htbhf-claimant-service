be1bfe3ad824b17561820c161830e55ebfb7fdb6
Gethyn Jones <gethyn1@gmail.com>
2019-03-19 11:34:55 +0000

    HTBHF-679 Handle rest exceptions in eligibility client. Ensure claimant is always saved, even on exceptions

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/service/EligibilityClientTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/service/EligibilityClientTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.dhsc.htbhf.claimant.entity.Claimant;
 import uk.gov.dhsc.htbhf.claimant.exception.EligibilityClientException;
@@ -72,6 +73,22 @@ class EligibilityClientTest {
 
         assertThat(thrown).as("Should throw an Exception when response code is not OK").isNotNull();
         assertThat(thrown.getMessage()).isEqualTo("Response code from Eligibility service was not OK, received: 400");
+        verify(claimantToPersonDTOConverter).convert(claimant);
+        verify(restTemplateWithIdHeaders).postForEntity(baseUri + ELIGIBILITY_ENDPOINT, person, EligibilityResponse.class);
+    }
+
+    @Test
+    void shouldThrowAnExceptionWhenPostCallReturnsError() {
+        Claimant claimant = aValidClaimant();
+        PersonDTO person = aValidPerson();
+        given(claimantToPersonDTOConverter.convert(any())).willReturn(person);
+        given(restTemplateWithIdHeaders.postForEntity(anyString(), any(), eq(EligibilityResponse.class)))
+                .willThrow(new RestClientException("Test exception"));
+
+        EligibilityClientException thrown = catchThrowableOfType(() -> client.checkEligibility(claimant), EligibilityClientException.class);
+
+        assertThat(thrown).as("Should throw an Exception when post call returns error").isNotNull();
+        assertThat(thrown.getMessage()).isEqualTo("Exception caught trying to call eligibility service at: " + baseUri + ELIGIBILITY_ENDPOINT);
         verify(claimantToPersonDTOConverter).convert(claimant);
         verify(restTemplateWithIdHeaders).postForEntity(baseUri + ELIGIBILITY_ENDPOINT, person, EligibilityResponse.class);
     }
