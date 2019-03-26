269084fb4ea01b517c2a49a378565a94262423d9
Gethyn Jones <gethyn1@gmail.com>
2019-03-26 16:44:59 +0000

    HTBHF-849 return response entity from endpoint

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/controller/ClaimController.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/controller/ClaimController.java
@@ -1,26 +1,22 @@
 package uk.gov.dhsc.htbhf.claimant.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.dhsc.htbhf.claimant.converter.ClaimDTOToClaimConverter;
 import uk.gov.dhsc.htbhf.claimant.entity.Claim;
+import uk.gov.dhsc.htbhf.claimant.entity.Claimant;
 import uk.gov.dhsc.htbhf.claimant.model.ClaimDTO;
 import uk.gov.dhsc.htbhf.claimant.service.ClaimService;
 import uk.gov.dhsc.htbhf.errorhandler.ErrorResponse;
 
 import javax.validation.Valid;
 
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController()
@@ -34,12 +30,13 @@ public class ClaimController {
     private final ClaimDTOToClaimConverter converter;
 
     @PostMapping
-    @ResponseStatus(CREATED)
     @ApiOperation("Persist a new claim.")
     @ApiResponses({@ApiResponse(code = 400, message = "Bad request", response = ErrorResponse.class)})
-    public void newClaim(@RequestBody @Valid @ApiParam("The claim to persist") ClaimDTO claimDTO) {
+    public ResponseEntity newClaim(@RequestBody @Valid @ApiParam("The claim to persist") ClaimDTO claimDTO) {
         log.debug("Received claim");
         Claim claim = converter.convert(claimDTO);
-        claimService.createClaim(claim);
+        Claimant claimant = claimService.createClaim(claim);
+
+        return claimService.createResponseFromClaimant(claimant);
     }
 }
--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/service/ClaimService.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/service/ClaimService.java
@@ -2,12 +2,17 @@ package uk.gov.dhsc.htbhf.claimant.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.dhsc.htbhf.claimant.entity.Claim;
 import uk.gov.dhsc.htbhf.claimant.entity.Claimant;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimResponse;
 import uk.gov.dhsc.htbhf.claimant.model.eligibility.EligibilityResponse;
 import uk.gov.dhsc.htbhf.claimant.model.eligibility.EligibilityStatus;
 import uk.gov.dhsc.htbhf.claimant.repository.ClaimantRepository;
+
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -17,8 +22,12 @@ public class ClaimService {
 
     private final ClaimantRepository claimantRepository;
     private final EligibilityClient client;
+    private final Map<EligibilityStatus, HttpStatus> statusMap = Map.of(
+      EligibilityStatus.ELIGIBLE, HttpStatus.CREATED,
+      EligibilityStatus.DUPLICATE, HttpStatus.OK
+    );
 
-    public void createClaim(Claim claim) {
+    public Claimant createClaim(Claim claim) {
         Claimant claimant = claim.getClaimant();
         EligibilityStatus eligibilityStatus = EligibilityStatus.ERROR;
 
@@ -34,6 +43,14 @@ public class ClaimService {
             claimant.setEligibilityStatus(eligibilityStatus);
             claimantRepository.save(claimant);
             log.info("Saved new claimant: {} with status {}", claimant.getId(), claimant.getEligibilityStatus());
+            return claimant;
         }
+    }
+
+    public ResponseEntity createResponseFromClaimant(Claimant claimant) {
+        EligibilityStatus eligibilityStatus = claimant.getEligibilityStatus();
+        HttpStatus statusCode = statusMap.get(eligibilityStatus);
+        ClaimResponse body = ClaimResponse.builder().eligibilityStatus(eligibilityStatus).build();
+        return new ResponseEntity(body, statusCode);
     }
 }
