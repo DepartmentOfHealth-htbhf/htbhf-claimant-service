902ebfa65db5cb30342af5f5982cb81b0a399c51
Gethyn Jones <gethyn1@gmail.com>
2019-04-08 16:59:41 +0100

    HTBHF-870 Refactor test constants

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/testsupport/EligibilityResponseTestDataFactory.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/testsupport/EligibilityResponseTestDataFactory.java
@@ -3,12 +3,11 @@ package uk.gov.dhsc.htbhf.claimant.testsupport;
 import uk.gov.dhsc.htbhf.claimant.model.eligibility.EligibilityResponse;
 import uk.gov.dhsc.htbhf.eligibility.model.EligibilityStatus;
 
+import static uk.gov.dhsc.htbhf.claimant.testsupport.TestConstants.DWP_HOUSEHOLD_IDENTIFIER;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.TestConstants.HMRC_HOUSEHOLD_IDENTIFIER;
 import static uk.gov.dhsc.htbhf.eligibility.model.EligibilityStatus.ELIGIBLE;
 
 public class EligibilityResponseTestDataFactory {
-
-    private static final String DWP_HOUSEHOLD_IDENTIFIER = "dwpHousehold1";
-    private static final String HMRC_HOUSEHOLD_IDENTIFIER = "hmrcHousehold1";
 
     public static EligibilityResponse anEligibilityResponse() {
         return aValidEligibilityResponseBuilder().build();
@@ -17,8 +16,8 @@ public class EligibilityResponseTestDataFactory {
     public static EligibilityResponse anEligibilityResponseWithStatus(EligibilityStatus eligibilityStatus) {
         return EligibilityResponse.builder()
                 .eligibilityStatus(eligibilityStatus)
-                .dwpHouseholdIdentifier("dwpHousehold1")
-                .hmrcHouseholdIdentifier("hmrcHousehold1")
+                .dwpHouseholdIdentifier(DWP_HOUSEHOLD_IDENTIFIER)
+                .hmrcHouseholdIdentifier(HMRC_HOUSEHOLD_IDENTIFIER)
                 .build();
     }
 
