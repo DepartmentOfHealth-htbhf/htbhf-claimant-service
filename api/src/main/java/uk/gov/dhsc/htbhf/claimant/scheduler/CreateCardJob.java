b925a722fc17340253a1eadd1e557c15638a94e9
David Wybourn <david.wybourn@gmail.com>
2019-04-23 15:27:35 +0100

    HTBHF-1164 Added schedule to message processor.

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/scheduler/CreateCardJob.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/scheduler/CreateCardJob.java
@@ -2,8 +2,6 @@ package uk.gov.dhsc.htbhf.claimant.scheduler;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.javacrumbs.shedlock.core.SchedulerLock;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.dhsc.htbhf.claimant.repository.ClaimRepository;
 import uk.gov.dhsc.htbhf.claimant.service.NewCardService;
@@ -19,11 +17,6 @@ public class CreateCardJob {
     private ClaimRepository claimRepository;
     private NewCardService newCardService;
 
-    @Scheduled(cron = "${card.schedule.new-card-interval-cron}")
-    @SchedulerLock(
-            name = "CreateNewCardsJob",
-            lockAtLeastForString = "${card.schedule.minimum-lock-time}",
-            lockAtMostForString = "${card.schedule.maximum-lock-time}")
     public void createNewCards() {
         log.info("Starting scheduled job to create new cards");
 
