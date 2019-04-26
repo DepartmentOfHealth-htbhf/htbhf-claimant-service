0f2782e15a7885364c3cf9d22029a6c7a5fd2689
David Wybourn <david.wybourn@gmail.com>
2019-04-26 17:34:33 +0100

    HTBHF-1185 Made calculation periods configurable.

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/entitlement/CycleEntitlementCalculator.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/entitlement/CycleEntitlementCalculator.java
@@ -1,31 +1,44 @@
 package uk.gov.dhsc.htbhf.claimant.entitlement;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.dhsc.htbhf.claimant.entity.Claimant;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 @Component
-@AllArgsConstructor
 public class CycleEntitlementCalculator {
 
+    private final Integer entitlementCalculationDuration;
+    private Integer numberOfCalculationPeriods;
     private final EntitlementCalculator entitlementCalculator;
 
-    public int calculateEntitlementInPence(Claimant claimant, List<LocalDate> childrenDatesOfBirth) {
+    public CycleEntitlementCalculator(@Value("${payment-cycle.cycle-duration-in-days}") Integer paymentCycleDurationInDays,
+                                      @Value("${payment-cycle.number-of-calculation-periods}") Integer numberOfCalculationPeriods,
+                                      EntitlementCalculator entitlementCalculator) {
+        // TODO check numbers are divisible
+        this.entitlementCalculationDuration = paymentCycleDurationInDays / numberOfCalculationPeriods;
+        this.numberOfCalculationPeriods = numberOfCalculationPeriods;
+        this.entitlementCalculator = entitlementCalculator;
+    }
 
-        List<LocalDate> entitlementDates = asList(
-                LocalDate.now(),
-                LocalDate.now().plusDays(7),
-                LocalDate.now().plusDays(14),
-                LocalDate.now().plusDays(21));
+    public int calculateEntitlementInPence(Claimant claimant, List<LocalDate> childrenDatesOfBirth) {
+        LocalDate today = LocalDate.now();
+        List<LocalDate> entitlementDates = getEntitlementDates(today);
 
         return entitlementDates.stream()
                 .map(date -> entitlementCalculator.calculateVoucherEntitlement(claimant, childrenDatesOfBirth, date))
                 .mapToInt(VoucherEntitlement::getTotalVoucherValueInPence)
                 .sum();
+    }
+
+    private List<LocalDate> getEntitlementDates(LocalDate today) {
+        List<LocalDate> entitlementDates = new ArrayList<>(numberOfCalculationPeriods);
+        for(int i = 0; i < numberOfCalculationPeriods; i++) {
+            entitlementDates.add(today.plusDays(i * entitlementCalculationDuration));
+        }
+        return entitlementDates;
     }
 }
