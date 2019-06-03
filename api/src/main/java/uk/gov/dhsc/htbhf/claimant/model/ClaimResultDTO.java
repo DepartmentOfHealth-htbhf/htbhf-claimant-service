f382807aa983f4fe418cc8abd85e1bd02cb09df7
Matt Smith <msmith@equalexperts.com>
2019-06-03 19:03:26 +0100

    NO-JIRA added api docs for ClaimResult

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/model/ClaimResultDTO.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/model/ClaimResultDTO.java
@@ -2,6 +2,7 @@ package uk.gov.dhsc.htbhf.claimant.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,17 +16,22 @@ import java.util.List;
 public class ClaimResultDTO {
 
     @JsonProperty("eligibilityStatus")
+    @ApiModelProperty(notes = "The eligibility status of the claim")
     private EligibilityStatus eligibilityStatus;
 
     @JsonProperty("claimStatus")
+    @ApiModelProperty(notes = "The status of the claim")
     private ClaimStatus claimStatus;
 
     @JsonProperty("voucherEntitlement")
+    @ApiModelProperty(notes = "Details of the vouchers that the claimant is entitled to")
     private VoucherEntitlementDTO voucherEntitlement;
 
     @JsonProperty("claimUpdated")
+    @ApiModelProperty(notes = "True if an existing, active, claim was updated. May be null if no existing claim was found.")
     private Boolean claimUpdated;
 
     @JsonProperty("updatedFields")
+    @ApiModelProperty(notes = "List of fields that were updated in the existing claim. May be null if no existing claim was found.")
     private List<String> updatedFields;
 }
