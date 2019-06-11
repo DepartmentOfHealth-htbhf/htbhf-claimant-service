0581a7f5f05e356c3b188b1c5d1c34324e6a7214
Matt Smith <msmith@equalexperts.com>
2019-06-11 09:27:49 +0100

    HTBHF-1533 Added a justification to SuppressFBWarnings

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/service/ClaimService.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/service/ClaimService.java
@@ -124,7 +124,8 @@ public class ClaimService {
         messageQueueDAO.sendMessage(payload, CREATE_NEW_CARD);
     }
 
-    @SuppressFBWarnings("SECMD5") // ignore spotbugs warning about md5 as we're not using it for encryption
+    @SuppressFBWarnings(value = "SECMD5",
+            justification = "Using a hash of the device fingerprint to identify multiple claims from the same device, not for encryption")
     private Claim buildClaim(Claimant claimant, EligibilityAndEntitlementDecision decision, Map<String, Object> deviceFingerprint) {
         ClaimStatus claimStatus = STATUS_MAP.get(decision.getEligibilityStatus());
         LocalDateTime currentDateTime = LocalDateTime.now();
