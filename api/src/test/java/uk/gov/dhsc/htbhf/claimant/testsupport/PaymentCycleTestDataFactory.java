1f718df02035f14e1525eca285d3f4886c3352be
Mike Smith <mike.smith@sescon.co.uk>
2019-07-26 07:57:45 +0100

    HTBHF-1883 Improve PaymentCycleTestDataFactory so that it creates voucher entitlements based on the start date and remove instances where this needed to be done manually in the code.

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/testsupport/PaymentCycleTestDataFactory.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/testsupport/PaymentCycleTestDataFactory.java
@@ -11,37 +11,51 @@ import java.time.LocalDate;
 
 import static uk.gov.dhsc.htbhf.claimant.entity.PaymentCycleStatus.NEW;
 import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.aValidClaim;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.PaymentCycleVoucherEntitlementTestDataFactory.aPaymentCycleVoucherEntitlementWithPregnancyVouchers;
 import static uk.gov.dhsc.htbhf.claimant.testsupport.PaymentCycleVoucherEntitlementTestDataFactory.aPaymentCycleVoucherEntitlementWithVouchers;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.PaymentCycleVoucherEntitlementTestDataFactory.aPaymentCycleVoucherEntitlementWithVouchersFromDate;
 
 public class PaymentCycleTestDataFactory {
 
-    private static final int TOTAL_VOUCHERS = 1;
+    private static final int TOTAL_VOUCHERS = 16;
     public static final int TOTAL_ENTITLEMENT_AMOUNT_IN_PENCE = 4960;
-
 
     public static PaymentCycle aValidPaymentCycle() {
         return aValidPaymentCycleBuilder().build();
     }
 
-    public static PaymentCycle aPaymentCycleWithCycleStartDateEntitlementAndClaim(LocalDate startDate,
-                                                                                  PaymentCycleVoucherEntitlement paymentCycleVoucherEntitlement,
-                                                                                  Claim claim) {
+    public static PaymentCycle aPaymentCycleWithCycleEntitlementAndClaim(PaymentCycleVoucherEntitlement paymentCycleVoucherEntitlement,
+                                                                         Claim claim) {
         return aValidPaymentCycleBuilder()
-                .cycleStartDate(startDate)
                 .voucherEntitlement(paymentCycleVoucherEntitlement)
                 .claim(claim)
                 .build();
     }
 
-    public static PaymentCycle aPaymentCycleWithCycleEntitlementAndClaim(PaymentCycleVoucherEntitlement paymentCycleVoucherEntitlement,
-                                                                         Claim claim) {
-        return aPaymentCycleWithCycleStartDateEntitlementAndClaim(LocalDate.now(), paymentCycleVoucherEntitlement, claim);
+    public static PaymentCycle aPaymentCycleWithStartDateAndClaim(LocalDate startDate,
+                                                                  Claim claim) {
+        return aValidPaymentCycleBuilder()
+                .voucherEntitlement(aPaymentCycleVoucherEntitlementWithVouchersFromDate(startDate))
+                .cycleStartDate(startDate)
+                .claim(claim)
+                .build();
     }
 
     public static PaymentCycle aPaymentCycleWithStartAndEndDate(LocalDate startDate, LocalDate endDate) {
         return aValidPaymentCycleBuilder()
                 .cycleStartDate(startDate)
+                .voucherEntitlement(aPaymentCycleVoucherEntitlementWithVouchersFromDate(startDate))
                 .cycleEndDate(endDate)
+                .build();
+    }
+
+    public static PaymentCycle aPaymentCycleWithPregnancyVouchersOnly(LocalDate startDate, LocalDate endDate) {
+        return aValidPaymentCycleBuilder()
+                .voucherEntitlement(aPaymentCycleVoucherEntitlementWithPregnancyVouchers())
+                .cycleStartDate(startDate)
+                .cycleEndDate(endDate)
+                .totalEntitlementAmountInPence(1240)
+                .totalVouchers(4)
                 .build();
     }
 
