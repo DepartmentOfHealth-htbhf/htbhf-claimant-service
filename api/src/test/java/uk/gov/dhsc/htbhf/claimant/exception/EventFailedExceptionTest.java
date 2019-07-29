3f3e60f2f545ecfd80ec9d265c3f14a91910ccbf
Mike Smith <mike.smith@sescon.co.uk>
2019-07-29 16:11:38 +0100

    HTBHF-1885 Refactor common verifications on event failures into a common test utility class.

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/exception/EventFailedExceptionTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/exception/EventFailedExceptionTest.java
@@ -1,43 +1,29 @@
 package uk.gov.dhsc.htbhf.claimant.exception;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.dhsc.htbhf.claimant.entity.Claim;
 import uk.gov.dhsc.htbhf.claimant.service.audit.NewCardEvent;
-import uk.gov.dhsc.htbhf.logging.event.CommonEventType;
-import uk.gov.dhsc.htbhf.logging.event.FailureEvent;
 
-import java.util.Map;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.dhsc.htbhf.logging.event.FailureEvent.EXCEPTION_DETAIL_KEY;
-import static uk.gov.dhsc.htbhf.logging.event.FailureEvent.FAILED_EVENT_KEY;
-import static uk.gov.dhsc.htbhf.logging.event.FailureEvent.FAILURE_DESCRIPTION_KEY;
+import static uk.gov.dhsc.htbhf.claimant.service.audit.FailedEventTestUtils.verifyCardCreationEventFailExceptionAndEventAreCorrect;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.aValidClaim;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.TestConstants.CARD_ACCOUNT_ID;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.TestConstants.TEST_EXCEPTION;
 
 class EventFailedExceptionTest {
 
     @Test
     void shouldCreateFailureEventOnConstruction() {
         //Given
-        UUID claimId = UUID.randomUUID();
-        NewCardEvent event = NewCardEvent.builder().claimId(claimId).build();
-        String failureMessage = "Something went badly wrong";
+        Claim claim = aValidClaim();
+        NewCardEvent event = NewCardEvent.builder().claimId(claim.getId()).cardAccountId(CARD_ACCOUNT_ID).build();
+        String failureMessage = String.format("Card creation failed for claim %s, exception is: test exception", claim.getId());
         RuntimeException testException = new RuntimeException("test exception");
 
         //When
         EventFailedException exception = new EventFailedException(event, testException, failureMessage);
 
         //Then
-        assertThat(exception).hasMessage(failureMessage);
-        assertThat(exception).hasCause(testException);
-        FailureEvent failureEvent = exception.getFailureEvent();
-        assertThat(failureEvent.getEventType()).isEqualTo(CommonEventType.FAILURE);
-        assertThat(failureEvent.getTimestamp()).isEqualTo(event.getTimestamp());
-        Map<String, Object> metadata = failureEvent.getEventMetadata();
-        assertThat(metadata).containsAllEntriesOf(event.getEventMetadata());
-        assertThat(metadata.get(FAILED_EVENT_KEY)).isEqualTo(event.getEventType());
-        assertThat(metadata.get(FAILURE_DESCRIPTION_KEY)).isEqualTo(failureMessage);
-        String actualExceptionDetail = (String) metadata.get(EXCEPTION_DETAIL_KEY);
-        assertThat(actualExceptionDetail).startsWith("test exception");
-        assertThat(actualExceptionDetail).contains("EventFailedExceptionTest.shouldCreateFailureEventOnConstruction");
+        verifyCardCreationEventFailExceptionAndEventAreCorrect(claim, TEST_EXCEPTION, exception,
+                "EventFailedExceptionTest.shouldCreateFailureEventOnConstruction", CARD_ACCOUNT_ID);
     }
 }
--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/testsupport/FailureEventTestDataFactory.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/testsupport/FailureEventTestDataFactory.java
@@ -3,13 +3,15 @@ package uk.gov.dhsc.htbhf.claimant.testsupport;
 import uk.gov.dhsc.htbhf.logging.event.Event;
 import uk.gov.dhsc.htbhf.logging.event.FailureEvent;
 
+import static uk.gov.dhsc.htbhf.claimant.testsupport.TestConstants.TEST_EXCEPTION;
+
 public class FailureEventTestDataFactory {
 
     public static FailureEvent aFailureEventWithEvent(Event event) {
         return FailureEvent.builder()
                 .failedEvent(event)
                 .failureDescription("my failure message")
-                .exception(new RuntimeException("test exception"))
+                .exception(TEST_EXCEPTION)
                 .build();
     }
 }
