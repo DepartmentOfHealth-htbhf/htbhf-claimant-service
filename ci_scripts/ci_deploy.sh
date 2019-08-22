b77821e4ff2a24edf3340c69e5960c6f4668b8e5
David Wybourn <david.wybourn@gmail.com>
2019-08-22 09:08:14 +0100

    HTBHF-2020 Switching over to circle ci.

--- ci_scripts/ci_deploy.sh
+++ ci_scripts/ci_deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# if this is a pull request or branch (non-master) build, then just exit
-echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST, TRAVIS_BRANCH=$TRAVIS_BRANCH"
-if [[ "$TRAVIS_PULL_REQUEST" != "false"  || "$TRAVIS_BRANCH" != "master" ]]; then
-   echo "Not deploying pull request or branch build"
-   exit
-fi
-
 check_variable_is_set(){
     if [[ -z ${!1} ]]; then
         echo "$1 must be set and non empty"
