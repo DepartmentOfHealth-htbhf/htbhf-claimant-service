59eac34a3d26efd4da14b3a92da2ffe8b262b78f
Matt Smith <msmith@equalexperts.com>
2019-09-17 16:34:06 +0100

    HTBHF-2241 Sending an email notification of regular payments

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/message/processor/MakePaymentMessageProcessorTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/message/processor/MakePaymentMessageProcessorTest.java
@@ -2,23 +2,31 @@ package uk.gov.dhsc.htbhf.claimant.message.processor;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.dhsc.htbhf.claimant.entity.Claim;
 import uk.gov.dhsc.htbhf.claimant.entity.Message;
 import uk.gov.dhsc.htbhf.claimant.entity.PaymentCycle;
+import uk.gov.dhsc.htbhf.claimant.message.MessageQueueClient;
 import uk.gov.dhsc.htbhf.claimant.message.MessageStatus;
+import uk.gov.dhsc.htbhf.claimant.message.MessageType;
 import uk.gov.dhsc.htbhf.claimant.message.context.MakePaymentMessageContext;
 import uk.gov.dhsc.htbhf.claimant.message.context.MessageContextLoader;
+import uk.gov.dhsc.htbhf.claimant.message.payload.EmailMessagePayload;
+import uk.gov.dhsc.htbhf.claimant.message.payload.EmailType;
+import uk.gov.dhsc.htbhf.claimant.message.payload.MessagePayload;
 import uk.gov.dhsc.htbhf.claimant.service.payments.PaymentService;
 import uk.gov.dhsc.htbhf.logging.event.FailureEvent;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.verify;
 import static org.mockito.Mockito.mock;
+import static uk.gov.dhsc.htbhf.claimant.message.EmailPayloadAssertions.assertEmailPayloadCorrectForClaimantWithAllVouchers;
 import static uk.gov.dhsc.htbhf.claimant.message.MessageType.MAKE_PAYMENT;
 import static uk.gov.dhsc.htbhf.claimant.testsupport.MessageContextTestDataFactory.aValidMakePaymentMessageContext;
 import static uk.gov.dhsc.htbhf.claimant.testsupport.MessageTestDataFactory.aValidMessageWithType;
@@ -31,6 +39,8 @@ class MakePaymentMessageProcessorTest {
     private PaymentService paymentService;
     @Mock
     private MessageContextLoader messageContextLoader;
+    @Mock
+    private MessageQueueClient messageQueueClient;
 
     @InjectMocks
     MakePaymentMessageProcessor processor;
@@ -48,6 +58,18 @@ class MakePaymentMessageProcessorTest {
         assertThat(result).isEqualTo(MessageStatus.COMPLETED);
         verify(messageContextLoader).loadMakePaymentContext(message);
         verify(paymentService).makePaymentForCycle(paymentCycle, claim.getCardAccountId());
+        verifyEmailNotificationSent(paymentCycle, claim);
+    }
+
+    private void verifyEmailNotificationSent(PaymentCycle paymentCycle, Claim claim) {
+        ArgumentCaptor<MessagePayload> payloadCaptor = ArgumentCaptor.forClass(MessagePayload.class);
+        verify(messageQueueClient).sendMessage(payloadCaptor.capture(), eq(MessageType.SEND_EMAIL));
+
+        assertThat(payloadCaptor.getValue()).isInstanceOf(EmailMessagePayload.class);
+        EmailMessagePayload payload = (EmailMessagePayload) payloadCaptor.getValue();
+        assertThat(payload.getEmailType()).isEqualTo(EmailType.PAYMENT);
+        assertThat(payload.getClaimId()).isEqualTo(claim.getId());
+        assertEmailPayloadCorrectForClaimantWithAllVouchers(payload.getEmailPersonalisation(), paymentCycle.getCycleEndDate().plusDays(1));
     }
 
     @Test
