4ed450c72d99a720f0edfb4afd1b96cc91fc6cd4
Mike Smith <mike.smith@sescon.co.uk>
2019-09-23 10:12:16 +0100

    HTBHF-2289 Move Notify template ids out of config into the enum to remove complexity on something which isn't going to change across environment.

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/EmailMessageConfiguration.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/EmailMessageConfiguration.java
@@ -1,31 +1,12 @@
 package uk.gov.dhsc.htbhf.claimant;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.gov.dhsc.htbhf.claimant.message.payload.EmailType;
 import uk.gov.service.notify.NotificationClient;
 
-import java.util.Map;
-
-@EnableConfigurationProperties
 @Configuration
-@ConfigurationProperties(prefix = "notify")
 public class EmailMessageConfiguration {
-
-    //TODO MRS 20/09/2019: Remove the template ids from the configuration, they should really sit with the enum and is too hidden in the config.
-    @Getter
-    @Setter
-    private Map<EmailType, String> templateIds;
-
-    @Bean
-    public EmailTemplateConfig emailTemplateConfig() {
-        return new EmailTemplateConfig(templateIds);
-    }
 
     @Bean
     public NotificationClient notificationClient(@Value("${notify.api-key}") String notifyApiKey) {
