cd75a834eb7bd44facb99148f54a62567f1f880f
Matt Smith <msmith@equalexperts.com>
2019-10-17 16:33:56 +0100

    HTBHF-2480 Made claimStatus private to claim, added updateClaimStatus method

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/entity/ClaimTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/entity/ClaimTest.java
@@ -4,13 +4,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.dhsc.htbhf.assertions.AbstractValidationTest;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimStatus;
+import uk.gov.dhsc.htbhf.eligibility.model.EligibilityStatus;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 import javax.validation.ConstraintViolation;
 
 import static uk.gov.dhsc.htbhf.assertions.ConstraintViolationAssert.assertThat;
-import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.*;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.aClaimWithClaimStatus;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.aClaimWithClaimant;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.aClaimWithEligibilityStatus;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimTestDataFactory.aValidClaim;
 import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimantTestDataFactory.aClaimantWithLastName;
 
 class ClaimTest extends AbstractValidationTest {
@@ -85,5 +91,59 @@ class ClaimTest extends AbstractValidationTest {
         ReflectionTestUtils.setField(claim, "id", id);
         //Then
         Assertions.assertThat(id).isEqualTo(claim.getId());
+    }
+
+    @Test
+    void shouldUpdateClaimStatusAndTimestamp() {
+        LocalDateTime originalTimestamp = LocalDateTime.now().minusHours(1);
+        Claim claim = Claim.builder()
+                .claimStatus(ClaimStatus.NEW)
+                .claimStatusTimestamp(originalTimestamp)
+                .build();
+        LocalDateTime now = LocalDateTime.now();
+
+        claim.updateClaimStatus(ClaimStatus.ACTIVE);
+
+        Assertions.assertThat(claim.getClaimStatusTimestamp()).isAfterOrEqualTo(now);
+    }
+
+    @Test
+    void shouldNotUpdateClaimStatusTimestampWhenStatusUnchanged() {
+        LocalDateTime originalTimestamp = LocalDateTime.now().minusHours(1);
+        Claim claim = Claim.builder()
+                .claimStatus(ClaimStatus.ACTIVE)
+                .claimStatusTimestamp(originalTimestamp)
+                .build();
+
+        claim.updateClaimStatus(ClaimStatus.ACTIVE);
+
+        Assertions.assertThat(claim.getClaimStatusTimestamp()).isEqualTo(originalTimestamp);
+    }
+
+    @Test
+    void shouldUpdateEligibilityStatusAndTimestamp() {
+        LocalDateTime originalTimestamp = LocalDateTime.now().minusHours(1);
+        Claim claim = Claim.builder()
+                .eligibilityStatus(EligibilityStatus.INELIGIBLE)
+                .eligibilityStatusTimestamp(originalTimestamp)
+                .build();
+        LocalDateTime now = LocalDateTime.now();
+
+        claim.updateEligibilityStatus(EligibilityStatus.ELIGIBLE);
+
+        Assertions.assertThat(claim.getEligibilityStatusTimestamp()).isAfterOrEqualTo(now);
+    }
+
+    @Test
+    void shouldNotUpdateEligibilityStatusTimestampWhenStatusUnchanged() {
+        LocalDateTime originalTimestamp = LocalDateTime.now().minusHours(1);
+        Claim claim = Claim.builder()
+                .eligibilityStatus(EligibilityStatus.ELIGIBLE)
+                .eligibilityStatusTimestamp(originalTimestamp)
+                .build();
+
+        claim.updateEligibilityStatus(EligibilityStatus.ELIGIBLE);
+
+        Assertions.assertThat(claim.getEligibilityStatusTimestamp()).isEqualTo(originalTimestamp);
     }
 }
