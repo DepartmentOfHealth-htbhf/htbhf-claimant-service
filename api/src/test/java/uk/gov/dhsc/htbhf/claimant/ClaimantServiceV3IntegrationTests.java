6b9ab0fa721f04be11a75b6d28ead0928f3401ab
Mike Smith <mike.smith@sescon.co.uk>
2019-11-26 11:53:49 +0000

    HTBHF-2571 Create simplest successful integration test for v3 controller.

---
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/ClaimantServiceV3IntegrationTests.java
@@ -0,0 +1,104 @@
+package uk.gov.dhsc.htbhf.claimant;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.ResponseEntity;
+import uk.gov.dhsc.htbhf.claimant.entity.Claim;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimDTO;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimResultDTO;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimStatus;
+import uk.gov.dhsc.htbhf.claimant.repository.ClaimRepository;
+import uk.gov.dhsc.htbhf.claimant.testsupport.RepositoryMediator;
+import uk.gov.dhsc.htbhf.dwp.model.v2.IdentityAndEligibilityResponse;
+import uk.gov.dhsc.htbhf.eligibility.model.EligibilityStatus;
+
+import java.net.URI;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.CREATED;
+import static uk.gov.dhsc.htbhf.claimant.ClaimantServiceAssertionUtils.assertClaimantMatchesClaimantDTO;
+import static uk.gov.dhsc.htbhf.claimant.ClaimantServiceAssertionUtils.buildClaimRequestEntityForUri;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.ClaimDTOTestDataFactory.aValidClaimDTOWithNoNullFields;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.IdAndEligibilityResponseTestDataFactory.anAllMatchedEligibilityConfirmedUCResponseWithHouseholdIdentifier;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.VoucherEntitlementDTOTestDataFactory.aValidVoucherEntitlementDTO;
+import static uk.gov.dhsc.htbhf.eligibility.model.EligibilityStatus.ELIGIBLE;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureEmbeddedDatabase
+@AutoConfigureWireMock(port = 8100)
+class ClaimantServiceV3IntegrationTests {
+
+    private static final URI CLAIMANT_ENDPOINT_URI_V3 = URI.create("/v3/claims");
+    private static final String ELIGIBILITY_SERVICE_URL = "/v2/eligibility";
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ClaimRepository claimRepository;
+
+    @Autowired
+    RepositoryMediator repositoryMediator;
+
+    @AfterEach
+    void cleanup() {
+        repositoryMediator.deleteAllEntities();
+        WireMock.reset();
+    }
+
+    @Test
+    void shouldAcceptAndCreateANewValidClaimWithNoNullFields() throws JsonProcessingException {
+        ClaimDTO claim = aValidClaimDTOWithNoNullFields();
+        //Given
+        IdentityAndEligibilityResponse identityAndEligibilityResponse = anAllMatchedEligibilityConfirmedUCResponseWithHouseholdIdentifier();
+        stubEligibilityServiceWithSuccessfulResponse(identityAndEligibilityResponse);
+        //When
+        ResponseEntity<ClaimResultDTO> response = restTemplate.exchange(buildClaimRequestEntityForUri(claim, CLAIMANT_ENDPOINT_URI_V3), ClaimResultDTO.class);
+        //Then
+        assertThatClaimResultHasNewClaim(response);
+        assertClaimPersistedSuccessfully(claim, ELIGIBLE, "dwpHousehold1");
+        verifyPostToEligibilityService();
+    }
+
+    private void assertThatClaimResultHasNewClaim(ResponseEntity<ClaimResultDTO> response) {
+        assertThat(response.getStatusCode()).isEqualTo(CREATED);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getClaimStatus()).isEqualTo(ClaimStatus.NEW);
+        assertThat(response.getBody().getEligibilityStatus()).isEqualTo(ELIGIBLE);
+        assertThat(response.getBody().getVoucherEntitlement()).isEqualTo(aValidVoucherEntitlementDTO());
+    }
+
+    private void assertClaimPersistedSuccessfully(ClaimDTO claimDTO,
+                                                  EligibilityStatus eligibilityStatus,
+                                                  String dwpHouseholdIdentifier) {
+        Iterable<Claim> claims = claimRepository.findAll();
+        assertThat(claims).hasSize(1);
+        Claim persistedClaim = claims.iterator().next();
+        assertClaimantMatchesClaimantDTO(claimDTO.getClaimant(), persistedClaim.getClaimant());
+        assertThat(persistedClaim.getEligibilityStatus()).isEqualTo(eligibilityStatus);
+        assertThat(persistedClaim.getDwpHouseholdIdentifier()).isEqualTo(dwpHouseholdIdentifier);
+        assertThat(persistedClaim.getHmrcHouseholdIdentifier()).isNull();
+    }
+
+    private void stubEligibilityServiceWithSuccessfulResponse(IdentityAndEligibilityResponse identityAndEligibilityResponse) throws JsonProcessingException {
+        String json = objectMapper.writeValueAsString(identityAndEligibilityResponse);
+        stubFor(post(urlEqualTo(ELIGIBILITY_SERVICE_URL)).willReturn(okJson(json)));
+    }
+
+    private void verifyPostToEligibilityService() {
+        verify(exactly(1), postRequestedFor(urlEqualTo(ELIGIBILITY_SERVICE_URL)));
+    }
+}
