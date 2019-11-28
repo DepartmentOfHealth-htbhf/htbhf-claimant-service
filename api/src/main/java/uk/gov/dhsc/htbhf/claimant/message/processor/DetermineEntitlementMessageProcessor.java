4b5cc3b5f58622db859810f6d89caee2d54e4b31
David Wybourn <dwybourn@users.noreply.github.com>
2019-11-28 10:54:59 +0000

    HTBHF-1758 Moved move determine entitlement logic into EligibilityDecisionHandler. (#447)

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/message/processor/DetermineEntitlementMessageProcessor.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/message/processor/DetermineEntitlementMessageProcessor.java
@@ -4,17 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.dhsc.htbhf.claimant.eligibility.EligibilityDecisionHandler;
-import uk.gov.dhsc.htbhf.claimant.entitlement.PregnancyEntitlementCalculator;
 import uk.gov.dhsc.htbhf.claimant.entity.Claim;
 import uk.gov.dhsc.htbhf.claimant.entity.Message;
 import uk.gov.dhsc.htbhf.claimant.entity.PaymentCycle;
 import uk.gov.dhsc.htbhf.claimant.message.*;
 import uk.gov.dhsc.htbhf.claimant.message.context.DetermineEntitlementMessageContext;
 import uk.gov.dhsc.htbhf.claimant.message.context.MessageContextLoader;
-import uk.gov.dhsc.htbhf.claimant.message.payload.MessagePayload;
 import uk.gov.dhsc.htbhf.claimant.model.eligibility.EligibilityAndEntitlementDecision;
-import uk.gov.dhsc.htbhf.claimant.repository.ClaimRepository;
-import uk.gov.dhsc.htbhf.claimant.service.ClaimMessageSender;
 import uk.gov.dhsc.htbhf.claimant.service.EligibilityAndEntitlementService;
 import uk.gov.dhsc.htbhf.claimant.service.payments.PaymentCycleService;
 
@@ -25,9 +21,7 @@ import javax.transaction.Transactional;
 import static uk.gov.dhsc.htbhf.claimant.message.MessageStatus.COMPLETED;
 import static uk.gov.dhsc.htbhf.claimant.message.MessageType.DETERMINE_ENTITLEMENT;
 import static uk.gov.dhsc.htbhf.claimant.model.ClaimStatus.ACTIVE;
-import static uk.gov.dhsc.htbhf.claimant.model.ClaimStatus.EXPIRED;
 import static uk.gov.dhsc.htbhf.claimant.model.ClaimStatus.PENDING_EXPIRY;
-import static uk.gov.dhsc.htbhf.claimant.reporting.ClaimAction.UPDATED_FROM_PENDING_EXPIRY_TO_EXPIRED;
 import static uk.gov.dhsc.htbhf.eligibility.model.EligibilityStatus.ELIGIBLE;
 
 @Slf4j
@@ -38,31 +32,19 @@ public class DetermineEntitlementMessageProcessor implements MessageTypeProcesso
     private final EligibilityAndEntitlementService eligibilityAndEntitlementService;
     private final MessageContextLoader messageContextLoader;
     private final PaymentCycleService paymentCycleService;
-    private final MessageQueueClient messageQueueClient;
     private final EligibilityDecisionHandler eligibilityDecisionHandler;
-    private final PregnancyEntitlementCalculator pregnancyEntitlementCalculator;
-    private final ClaimMessageSender claimMessageSender;
-    private final ClaimRepository claimRepository;
 
     public DetermineEntitlementMessageProcessor(
             @Value("${payment-cycle.maximum-pending-expiry-duration}") Period maximumPendingExpiryDuration,
             EligibilityAndEntitlementService eligibilityAndEntitlementService,
             MessageContextLoader messageContextLoader,
             PaymentCycleService paymentCycleService,
-            MessageQueueClient messageQueueClient,
-            EligibilityDecisionHandler eligibilityDecisionHandler,
-            PregnancyEntitlementCalculator pregnancyEntitlementCalculator,
-            ClaimMessageSender claimMessageSender,
-            ClaimRepository claimRepository) {
+            EligibilityDecisionHandler eligibilityDecisionHandler) {
         this.maximumPendingExpiryDuration = maximumPendingExpiryDuration;
         this.eligibilityAndEntitlementService = eligibilityAndEntitlementService;
         this.messageContextLoader = messageContextLoader;
         this.paymentCycleService = paymentCycleService;
-        this.messageQueueClient = messageQueueClient;
         this.eligibilityDecisionHandler = eligibilityDecisionHandler;
-        this.pregnancyEntitlementCalculator = pregnancyEntitlementCalculator;
-        this.claimMessageSender = claimMessageSender;
-        this.claimRepository = claimRepository;
     }
 
     @Override
@@ -101,17 +83,11 @@ public class DetermineEntitlementMessageProcessor implements MessageTypeProcesso
                                 EligibilityAndEntitlementDecision decision,
                                 LocalDateTime messageTimestamp) {
         if (decision.getEligibilityStatus() == ELIGIBLE) {
-            createMakePaymentMessage(currentPaymentCycle);
-            if (pregnancyEntitlementCalculator.currentCycleIsSecondToLastCycleWithPregnancyVouchers(currentPaymentCycle)) {
-                // Email is worded such that we don't need to check if a new child from pregnancy has already appeared.
-                claimMessageSender.sendReportABirthEmailMessage(claim);
-            }
+            eligibilityDecisionHandler.handleEligibleDecision(claim, currentPaymentCycle);
         } else if (claim.getClaimStatus() == ACTIVE) {
-            eligibilityDecisionHandler.handleIneligibleDecision(claim, previousPaymentCycle, currentPaymentCycle, decision);
+            eligibilityDecisionHandler.handleIneligibleDecisionForActiveClaim(claim, previousPaymentCycle, currentPaymentCycle, decision);
         } else if (claimHasBeenPendingExpiryForLongerThanTheMaximumDuration(claim, messageTimestamp)) {
-            claim.updateClaimStatus(EXPIRED);
-            claimRepository.save(claim);
-            claimMessageSender.sendReportClaimMessage(claim, decision.getDateOfBirthOfChildren(), UPDATED_FROM_PENDING_EXPIRY_TO_EXPIRED);
+            eligibilityDecisionHandler.expirePendingExpiryClaim(claim, decision.getDateOfBirthOfChildren());
         }
     }
 
@@ -119,10 +95,5 @@ public class DetermineEntitlementMessageProcessor implements MessageTypeProcesso
         // !isAfter has the same effect as (isBefore || isEqual). Unfortunately isBeforeOrEqual does not exist on LocalDateTime.
         return claim.getClaimStatus() == PENDING_EXPIRY
                 && !claim.getClaimStatusTimestamp().isAfter(messageTimestamp.minus(maximumPendingExpiryDuration));
-    }
-
-    private void createMakePaymentMessage(PaymentCycle paymentCycle) {
-        MessagePayload messagePayload = MessagePayloadFactory.buildMakePaymentMessagePayload(paymentCycle);
-        messageQueueClient.sendMessage(messagePayload, MessageType.MAKE_PAYMENT);
     }
 }
