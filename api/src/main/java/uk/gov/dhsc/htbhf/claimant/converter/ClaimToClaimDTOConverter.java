b1be897445f4f804d46ed7f25195e40da4596dc5
David Wybourn <david.wybourn@gmail.com>
2020-01-13 13:29:05 +0000

    AFGS-565 Added ClaimDTO class used for read/query operations.

---
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/converter/ClaimToClaimDTOConverter.java
@@ -0,0 +1,49 @@
+package uk.gov.dhsc.htbhf.claimant.converter;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.dhsc.htbhf.claimant.entity.Claim;
+import uk.gov.dhsc.htbhf.claimant.entity.Claimant;
+import uk.gov.dhsc.htbhf.claimant.model.AddressDTO;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimDTO;
+import uk.gov.dhsc.htbhf.claimant.model.ClaimantDTO;
+
+@Component
+@AllArgsConstructor
+public class ClaimToClaimDTOConverter {
+
+    private final AddressToAddressDTOConverter addressToAddressDTOConverter;
+
+    public ClaimDTO convert(Claim claim) {
+        return ClaimDTO.builder()
+                .id(claim.getId())
+                .cardAccountId(claim.getCardAccountId())
+                .cardStatus(claim.getCardStatus())
+                .cardStatusTimestamp(claim.getCardStatusTimestamp())
+                .claimStatus(claim.getClaimStatus())
+                .claimStatusTimestamp(claim.getClaimStatusTimestamp())
+                .currentIdentityAndEligibilityResponse(claim.getCurrentIdentityAndEligibilityResponse())
+                .dwpHouseholdIdentifier(claim.getDwpHouseholdIdentifier())
+                .eligibilityStatus(claim.getEligibilityStatus())
+                .eligibilityStatusTimestamp(claim.getEligibilityStatusTimestamp())
+                .hmrcHouseholdIdentifier(claim.getHmrcHouseholdIdentifier())
+                .initialIdentityAndEligibilityResponse(claim.getInitialIdentityAndEligibilityResponse())
+                .claimant(convert(claim.getClaimant()))
+                .build();
+    }
+
+    private ClaimantDTO convert(Claimant claimant) {
+        AddressDTO addressDTO = addressToAddressDTOConverter.convert(claimant.getAddress());
+        return ClaimantDTO.builder()
+                .initiallyDeclaredChildrenDob(claimant.getInitiallyDeclaredChildrenDob())
+                .dateOfBirth(claimant.getDateOfBirth())
+                .expectedDeliveryDate(claimant.getExpectedDeliveryDate())
+                .address(addressDTO)
+                .emailAddress(claimant.getEmailAddress())
+                .firstName(claimant.getFirstName())
+                .lastName(claimant.getLastName())
+                .nino(claimant.getNino())
+                .phoneNumber(claimant.getPhoneNumber())
+                .build();
+    }
+}
