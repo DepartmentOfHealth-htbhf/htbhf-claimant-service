63e98b937718804119a39506b48cd6185c6ab6b8
amarkumarjagtap <amar.v.jagtap@gmail.com>
2020-01-17 14:45:53 +0000

    addressed review comments

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/model/NewClaimDTOTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/model/NewClaimDTOTest.java
@@ -1,9 +1,7 @@
 package uk.gov.dhsc.htbhf.claimant.model;
 
-import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.dhsc.htbhf.assertions.AbstractValidationTest;
 import uk.gov.dhsc.htbhf.dwp.model.EligibilityOutcome;
