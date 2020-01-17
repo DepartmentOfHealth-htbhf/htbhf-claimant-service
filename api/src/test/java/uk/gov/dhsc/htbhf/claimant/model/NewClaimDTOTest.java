6f20fbac239224fdebb5f2d3172a8ba62be49c31
amarkumarjagtap <amar.v.jagtap@gmail.com>
2020-01-17 14:40:43 +0000

    addressed review comments

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/model/NewClaimDTOTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/model/NewClaimDTOTest.java
@@ -1,11 +1,16 @@
 package uk.gov.dhsc.htbhf.claimant.model;
 
+import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.dhsc.htbhf.assertions.AbstractValidationTest;
 import uk.gov.dhsc.htbhf.dwp.model.EligibilityOutcome;
 
 import java.time.LocalDate;
 import java.util.Set;
+import java.util.stream.Stream;
 import javax.validation.ConstraintViolation;
 
 import static uk.gov.dhsc.htbhf.TestConstants.NO_CHILDREN;
@@ -81,20 +86,28 @@ class NewClaimDTOTest extends AbstractValidationTest {
 
     }
 
-    @Test
-    void shouldFailToValidateClaimWithPastUntilDateOnEligibilityOverride() {
+    @ParameterizedTest
+    @MethodSource("datesNotInFuture")
+    void shouldFailToValidateClaimWithEligibilityOverrideUntilDatesNotInFuture(LocalDate untilDate) {
         //Given
         NewClaimDTO claim = aValidClaimDTOWithEligibilityOverride(
                 EXPECTED_DELIVERY_DATE_IN_TWO_MONTHS,
                 NO_CHILDREN,
                 EligibilityOutcome.CONFIRMED,
-                LocalDate.now());
+                untilDate);
         //When
         Set<ConstraintViolation<NewClaimDTO>> violations = validator.validate(claim);
         //Then
         assertThat(violations).hasTotalViolations(1);
         assertThat(violations).hasViolation("must be a future date", "eligibilityOverride.overrideUntil");
 
+    }
+
+    private static Stream<LocalDate> datesNotInFuture() {
+        return Stream.of(
+                LocalDate.now(),
+                LocalDate.now().minusDays(1)
+        );
     }
 
     @Test
