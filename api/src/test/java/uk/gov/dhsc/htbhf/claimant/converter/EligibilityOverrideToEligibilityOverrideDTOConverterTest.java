40e56def8b1f2e1d3a08f818de7fa7bb96d2dca4
amarkumarjagtap <amar.v.jagtap@gmail.com>
2020-01-22 09:40:35 +0000

    addressed review comments

--- api/src/test/java/uk/gov/dhsc/htbhf/claimant/converter/EligibilityOverrideToEligibilityOverrideDTOConverterTest.java
+++ api/src/test/java/uk/gov/dhsc/htbhf/claimant/converter/EligibilityOverrideToEligibilityOverrideDTOConverterTest.java
@@ -3,10 +3,12 @@ package uk.gov.dhsc.htbhf.claimant.converter;
 import org.junit.jupiter.api.Test;
 import uk.gov.dhsc.htbhf.claimant.entity.EligibilityOverride;
 import uk.gov.dhsc.htbhf.claimant.model.EligibilityOverrideDTO;
+import uk.gov.dhsc.htbhf.dwp.model.EligibilityOutcome;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static uk.gov.dhsc.htbhf.TestConstants.MAGGIE_AND_LISA_DOBS;
 import static uk.gov.dhsc.htbhf.claimant.testsupport.EligibilityOverrideTestDataFactory.aConfirmedEligibilityOverrideWithChildren;
+import static uk.gov.dhsc.htbhf.claimant.testsupport.TestConstants.OVERRIDE_UNTIL_FIVE_YEARS;
 
 class EligibilityOverrideToEligibilityOverrideDTOConverterTest {
 
@@ -22,9 +24,9 @@ class EligibilityOverrideToEligibilityOverrideDTOConverterTest {
 
         //Then
         assertThat(result).isNotNull();
-        assertThat(result.getEligibilityOutcome()).isEqualTo(eligibilityOverride.getEligibilityOutcome());
+        assertThat(result.getEligibilityOutcome()).isEqualTo(EligibilityOutcome.CONFIRMED);
         assertThat(result.getChildrenDob()).isEqualTo(MAGGIE_AND_LISA_DOBS);
-        assertThat(result.getOverrideUntil()).isEqualTo(eligibilityOverride.getOverrideUntil());
+        assertThat(result.getOverrideUntil()).isEqualTo(OVERRIDE_UNTIL_FIVE_YEARS);
     }
 
     @Test
