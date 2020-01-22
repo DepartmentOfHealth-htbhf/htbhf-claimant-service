a021b946a5ea1d241b47c4c203332f36122381dd
amarkumarjagtap <amar.v.jagtap@gmail.com>
2020-01-22 14:31:41 +0000

    using commons object mapper to store date format in json

--- api/src/main/java/uk/gov/dhsc/htbhf/claimant/entity/util/EntityObjectMapperSupplier.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/claimant/entity/util/EntityObjectMapperSupplier.java
@@ -1,14 +1,9 @@
 package uk.gov.dhsc.htbhf.claimant.entity.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.vladmihalcea.hibernate.type.util.ObjectMapperSupplier;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import static uk.gov.dhsc.htbhf.ObjectMapperFactory.configureObjectMapper;
 
 /**
  * Supplies an {@link ObjectMapper} that is used by the json hibernate type.
@@ -19,13 +14,6 @@ public class EntityObjectMapperSupplier implements ObjectMapperSupplier {
 
     @Override
     public ObjectMapper get() {
-        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
-
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
-        objectMapper.registerModule(javaTimeModule);
-
-        return objectMapper;
+        return configureObjectMapper();
     }
 }
